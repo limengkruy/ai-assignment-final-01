c66c22d06b48a8bab62a7f8af8f16ea61cbed845
limengkruy <42145098+limengkruy@users.noreply.github.com>
2025-04-04 02:08:04 +0700

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,12 @@
 # test_app.py
 import pytest
 from fastapi.testclient import TestClient
+import sys
+import os
+
+# Add the parent directory to the sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from main import app  # Import your FastAPI app
 
 client = TestClient(app)
